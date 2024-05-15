5eaee2997ca83e148361d2d4d04099edf13dc965
NguyenNhaTran803 <nguyennhatran803@gmail.com>
2024-05-15 14:41:46 +0700

    project new

---
+++ du_an_ca_nhan_coffee/src/main/java/com/poly/du_an_ca_nhan_coffee/DuAnCaNhanCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.poly.du_an_ca_nhan_coffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuAnCaNhanCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuAnCaNhanCoffeeApplication.class, args);
+	}
+
+}
---
+++ du_an_ca_nhan_coffee/src/test/java/com/poly/du_an_ca_nhan_coffee/DuAnCaNhanCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.du_an_ca_nhan_coffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuAnCaNhanCoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
